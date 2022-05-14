3a5c482e0961945bfea0e4dd9e67475fa0639a0c
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-05-14 21:55:35 +0430

    change serverbox component name to biobox

---
+++ src/components/BioBox.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="bio-box">
+    <img class="bio-box__pfp" src="/imgs/profilepic.jpg" alt="" />
+    <h3 class="bio-box__title">'Zer0Power</h3>
+    <p class="bio-box__subtitle">0powerdev.ir</p>
+    <br />
+    <p class="bio-box__bio">2003/12/27</p>
+    <br />
+    <p class="bio-box__bio">INTP</p>
+    <br />
+    <p class="bio-box__bio">US / IR</p>
+    <div class="bio-box__tags">
+      <tag>🎮 Gamer</tag>
+      <tag>🎵 Music</tag>
+      <tag>☁ In The Clouds</tag>
+      <tag>😇 Angel</tag>
+      <tag>⚠️ Ex-Pentester</tag>
+      <tag>🌗 MoonWalker</tag>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Tag from "@/components/Tag.vue";
+</script>
+
+<style lang="scss" scoped>
+.bio-box {
+  background: rgba(0, 0, 0, 0.5);
+  padding: 1rem;
+  border-radius: 0.5rem;
+  width: 300px;
+  color: white;
+
+  .bio-box__pfp {
+    display: block;
+    width: 40%;
+    margin: auto;
+    border-radius: 0.3rem;
+  }
+
+  .bio-box__title {
+    font-family: "Macondo", cursive;
+    font-size: 20px;
+    text-align: center;
+    color: white;
+    margin: 0.3rem;
+  }
+  .bio-box__subtitle {
+    font-family: "Roboto", sans-serif;
+    font-size: 15px;
+    color: #131415;
+    text-align: center;
+    margin: 0;
+  }
+  .bio-box__bio {
+    font-family: Modelica, sans-serif;
+    font-size: 15px;
+    color: #131415;
+    text-align: center;
+    margin: 0;
+  }
+  .bio-box__tags {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin: 1rem 0;
+    gap: 0.3rem;
+  }
+}
+</style>
