18608a6e5651b1083f4b862866589b5ebcf127e0
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-05-15 15:59:41 +0430

    create a basic store

--- src/pinia/main.js
+++ src/pinia/main.js
@@ -2,6 +2,11 @@ import { defineStore } from "pinia";
 
 export const mainStore = defineStore("main", {
   state: () => ({
-    
-  })
+    showBio: true,
+  }),
+  actions: {
+    toggleShowBio() {
+      this.showBio = !this.showBio;
+    }
+  }
 });
