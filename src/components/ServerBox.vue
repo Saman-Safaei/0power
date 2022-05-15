afb229929ed8cc0c2711cd0cf4f2003cdc7c469e
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-05-15 15:59:16 +0430

    add toggle button

--- src/components/ServerBox.vue
+++ src/components/ServerBox.vue
@@ -1,11 +1,19 @@
 <template>
-  <div>test</div>
+  <div>
+    <div>test</div>
+    <button @click="toggle">BioBox</button>
+  </div>
 </template>
 
 <script setup>
+import { mainStore } from '@/pinia/main';
 
+const store = mainStore();
+
+function toggle() {
+  store.toggleShowBio();
+}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
