e19d9f5aa67def42a48eabacfde40c8b03e621e2
Zer0Power <zeroopoower@gmail.com>
2022-05-16 18:33:10 +0430

    upload edited files

--- src/components/ServerBox.vue
+++ src/components/ServerBox.vue
@@ -2,12 +2,17 @@
   <div class="server-box">
     <button @click="toggle" class="server-box__toggle"><img src="/imgs/bioboxbtn.png"
         class="server-box__toggle-img"></button>
+
+    <h3 class="server-box__ptitle">Ports</h3>
+    <drop-down title="Ports 1" :data="dummyData.Ports1"/>
+    <drop-down title="Ports 2" :data="dummyData.Ports2"/>
   </div>
 </template>
 
 <script setup>
 import { mainStore } from '@/pinia/main';
 import { reactive } from 'vue';
+import DropDown from "@/components/ServerData/DropDown.vue";
 
 const store = mainStore();
 
@@ -17,7 +22,7 @@ const dummyData = reactive(
     Server1: 32,
     Server2: 37,
     Ports1: [27015, 27016, 27018, 27021, 28164, 28165, 28173, 28477, 28500, 28502, 28517, 28582, 28585, 28623, 28652, 28667, 28675, 28708, 28738, 28752, 28787, 28810, 28811, 28812, 28823, 28833, 28846, 28854, 28856, 28865],
-    Port2: [27015, 27016, 27018, 27021, 28164, 28165, 28173, 28477, 28500, 28502, 28517, 28582, 28585, 28623, 28652, 28667, 28675, 28708, 28738, 28752, 28787, 28810, 28811, 28812, 28823, 28833, 28846, 28854, 28856, 28865]
+    Ports2: [27015, 27016, 27018, 27021, 28164, 28165, 28173, 28477, 28500, 28502, 28517, 28582, 28585, 28623, 28652, 28667, 28675, 28708, 28738, 28752, 28787, 28810, 28811, 28812, 28823, 28833, 28846, 28854, 28856, 28865]
   });
 
 function toggle() {
@@ -34,6 +39,7 @@ function toggle() {
   border-radius: 0.5rem;
   padding: 1rem;
   position: relative;
+  margin: 0.8rem 0;
 
   .server-box__toggle {
     position: absolute;
@@ -45,6 +51,33 @@ function toggle() {
       width: 30px;
     }
 
+  }
+
+  .server-box__ptitle {
+    position: relative;
+    text-align: center;
+    color: rgb(40, 40, 40);
+
+    &::after {
+      content: "";
+      position: absolute;
+      width: 35%;
+      height: 2px;
+      top: 50%;
+      left: 8px;
+      background: rgb(40, 40, 40);
+      border-radius: 2px;
+    }
+    &::before {
+      content: "";
+      position: absolute;
+      width: 35%;
+      height: 2px;
+      top: 50%;
+      right: 8px;
+      background: rgb(40, 40, 40);
+      border-radius: 2px;
+    }
   }
 
 }
--- src/components/ServerData/DropDown.vue
+++ src/components/ServerData/DropDown.vue
@@ -1,13 +1,80 @@
 <template>
-
+  <div class="dropdown-container">
+    <div class="dropdown"><span>{{ props.title }}</span> <button @click="toggleOpen" :style="arrow"
+        style="transition: transform 300ms;">&#10148;</button></div>
+    <Transition name="content">
+      <div class="dropdown__content" v-if="isOpen">
+        <div class="content__row" v-for="(value, index) in data" :key="value">
+          <span>{{ index + 1 }}</span> <span>{{ value }}</span>
+        </div>
+      </div>
+    </Transition>
+  </div>
 </template>
 
 <script setup>
+import { ref, computed, reactive } from 'vue';
+
 const props = defineProps({
-  data: Array
+  data: Array,
+  title: String,
+});
+
+const isOpen = ref(false);
+
+function toggleOpen() {
+  isOpen.value = !isOpen.value;
+}
+
+const arrow = computed(() => {
+  return (isOpen.value) ? { transform: "rotateZ(90deg)" } : {};
 });
 </script>
 
 <style lang="scss" scoped>
+.dropdown-container {
+  color: rgb(40, 40, 40);
+  font-family: sans-serif;
+  position: relative;
 
+}
+
+.dropdown {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  border-radius: 0.5rem;
+  padding: 0.5rem 1rem;
+}
+
+.dropdown__content {
+  background: rgba(187, 187, 187, 0.8);
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  z-index: 1;
+
+  .content__row {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 0.3rem 1rem;
+  }
+}
+
+.content-enter-from,
+.content-leave-to {
+  opacity: 0;
+  transform: translateY(-1rem);
+}
+.content-enter-active,
+.content-leave-active {
+  transition: 300ms;
+}
 </style>
