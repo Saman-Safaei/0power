36e95b36d46406d7bf0071a2a5e98593e8adbd29
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-05-17 17:32:10 +0430

    remove docs folder

--- docs/assets/AboutView.922e73c9.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c as t,a as o}from"./index.406666e7.js";const _={},a={class:"about"},r=o("h1",null,"This is an about page",-1),c=[r];function i(n,u){return s(),t("div",a,c)}var l=e(_,[["render",i],["__file","C:/Users/Administrator/Projects/Vue/0power/src/views/AboutView.vue"]]);export{l as default};
